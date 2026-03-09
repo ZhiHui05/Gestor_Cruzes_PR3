87cdca3b25e2de42065fa6d32a3cc0f55ff1a67f
Víctor Lozoya Carballo <145371712+enderdisgrace@users.noreply.github.com>
2026-03-09 11:13:09 +0100

    Increase retroceso timeout from 6000ms to 12000ms

--- COCHE/coche_2.ino
+++ COCHE/coche_2.ino
@@ -59,7 +59,7 @@ Estado estadoPrev = SEGUIR_LINEA;
 /* ===== Control de timeouts ===== */
 unsigned long tEstado = 0;                        // tiempo de entrada al estado actual
 const unsigned long TIMEOUT_CRUZAR_MS = 5000;     // timeout para cruzar
-const unsigned long TIMEOUT_RETROCESO_MS = 6000;  // timeout para evitar quedar atrapado
+const unsigned long TIMEOUT_RETROCESO_MS = 12000;  // timeout para evitar quedar atrapado
 const unsigned long TIMEOUT_SOLICITUD_MS = 2000;  // reintentar solicitud cada 2s
 
 /* ===== Eventos (bitmask) ===== */
